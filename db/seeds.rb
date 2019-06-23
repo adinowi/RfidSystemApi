dfcac7554416fc8f95ee38bece76b7d225848658
Adrian Nowicki <adinowi@gmail.com>
2019-06-23 12:02:13 +0200

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 
-sensor = Sensor.create(deleted: false)
 user = User.create(email: 'test@adi-space.tk', password: 'test')
 butter = Product.create(name: 'butter', price: 1.5, deleted: false)
 cheese = Product.create(name: 'cheese', price: 2.79, deleted: false)
-tag_1 = Tag.create(product_id: butter.id, paid: false)
-tag_2 = Tag.create(product_id: butter.id, paid: false)
-tag_3 = Tag.create(product_id: cheese.id, paid: false)
-tag_4 = Tag.create(product_id: cheese.id, paid: false)
+
+i = 0
+num = 25
+
+while i < num  do
+    Tag.create(product_id: cheese.id, paid: false)
+   i +=1
+end
+i=0
+while i < num  do
+    Tag.create(product_id: butter.id, paid: false)
+   i +=1
+end
+i=0
+while i < 3  do
+    Sensor.create(deleted: false)
+    i +=1
+end
 
